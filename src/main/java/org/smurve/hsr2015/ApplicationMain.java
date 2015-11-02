0a72091ac5a0936e30c9c7510b75e9ea44ec3bf8
Wolfgang Giersche <wgiersche@gmail.com>
2015-11-02 23:14:53 +0100

    AccountRepo must be found for other test to succeed. This is the student's starting point

--- src/main/java/org/smurve/hsr2015/ApplicationMain.java
+++ src/main/java/org/smurve/hsr2015/ApplicationMain.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackageClasses = ApplicationMain.class)
-@EnableJpaRepositories(basePackageClasses = {BookRepo.class})
+@EnableJpaRepositories(basePackageClasses = {BookRepo.class, AccountRepo.class})
 public class ApplicationMain {
 
     public static void main(String[] args) {
