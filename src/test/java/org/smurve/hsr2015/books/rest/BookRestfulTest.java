099beff74ead2230ac81b66f7c01918341bce96e
Wolfgang Giersche <wgiersche@gmail.com>
2015-11-04 14:59:52 +0100

    Restfultest

---
+++ src/test/java/org/smurve/hsr2015/books/rest/BookRestfulTest.java
@@ -0,0 +1,28 @@
+package org.smurve.hsr2015.books.rest;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.smurve.hsr2015.ApplicationMain;
+import org.smurve.hsr2015.books.domain.Book;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ApplicationMain.class)
+public class BookRestfulTest {
+
+    RestTemplate template = new TestRestTemplate();
+
+    @Test
+    public void testBookService () {
+        Book[] result = template.getForObject("http://localhost:8080", Book[].class);
+
+        Assert.assertEquals(1, result.length);
+    }
+
+}
