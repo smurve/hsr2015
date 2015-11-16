020bf039fc94bd13b6e546c2591e2b3f3359c788
Wolfgang Giersche <wgiersche@gmail.com>
2015-11-16 22:00:04 +0100

    JOOQ generating into src/main/java

--- AdvancedPersistency/src/test/java/org/smurve/hsr2014/jooq/JooqTest.java
+++ AdvancedPersistency/src/test/java/org/smurve/hsr2014/jooq/JooqTest.java
@@ -2,9 +2,10 @@ package org.smurve.hsr2014.jooq;
 
 import org.jooq.*;
 import org.jooq.impl.DSL;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static test.generated.Tables.*;
+import static org.smurve.hsr2014.jooq.Tables.*;
 
 import java.sql.*;
 
@@ -23,43 +24,71 @@ public class JooqTest {
         // Connection is the only JDBC resource that we need
         // PreparedStatement and ResultSet are handled by jOOQ, internally
         try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+
             DSLContext ctx = DSL.using(conn, SQLDialect.MYSQL);
 
-            ctx.deleteFrom(BOOK).execute();
-            ctx.deleteFrom(ACCOUNT).execute();
-            ctx.deleteFrom(AUTHOR).execute();
+            deleteAll(ctx);
 
 
-            ctx.insertInto(AUTHOR, AUTHOR.ID, AUTHOR.FIRSTNAME, AUTHOR.LASTNAME, AUTHOR.CATEGORY)
-                    .values(10L, "Harry", "Hirsch", "Fiction").execute();
-
-            ctx.insertInto(ACCOUNT, ACCOUNT.ID, ACCOUNT.ACCOUNTNUMBER, ACCOUNT.ACCOUNTOWNER_ID, ACCOUNT.AMOUNT)
-                    .values(11L, "SAVINGS001", 10L, 1500.00).execute();
-
-            ctx.insertInto(BOOK, BOOK.ID, BOOK.AUTHOR_ID, BOOK.TITLE, BOOK.PRICE, BOOK.CATEGORY)
-                    .values(12L, 10L, "My little farm", 145.00, "Poetry").execute();
-
-            ctx.insertInto(BOOK, BOOK.ID, BOOK.AUTHOR_ID, BOOK.TITLE, BOOK.PRICE, BOOK.CATEGORY)
-                    .values(13L, 10L, "Your little farm", 146.00, "Poetry").execute();
+            insertSomeValues(ctx);
 
 
-            Result<Record> books = ctx.select().from(BOOK)
-                    .where(
-                            BOOK.AUTHOR_ID.equal(10L))
-                    .and(
-                            BOOK.TITLE.startsWith("My")
-                                    .or(BOOK.TITLE.startsWith("Your")))
-                    .fetch();
+            Result<Record> books = selectSomeRecords(ctx);
 
-            for (Record r : books) {
-                String title = r.getValue(BOOK.TITLE);
-                System.out.println(title);
-            }
+            assertTitles(books);
 
-        }
-        // For the sake of this tutorial, let's keep exception handling simple
-        catch (Exception e) {
+        } catch (Exception e) {
+
+            // For the sake of this tutorial, let's keep exception handling simple
             e.printStackTrace();
         }
+    }
+
+    private void assertTitles(Result<Record> books) {
+        Assert.assertTrue(books.stream().anyMatch((b) -> b.getValue(BOOK.TITLE).startsWith("My")));
+        Assert.assertTrue(books.stream().anyMatch((b) -> b.getValue(BOOK.TITLE).startsWith("Your")));
+    }
+
+    /**
+     * Select all book titles starting with "My" or "Your"
+     * @param ctx the DSL Context to use
+     * @return the resulting list of records
+     */
+    private Result<Record> selectSomeRecords(DSLContext ctx) {
+        return ctx.select().from(BOOK)
+                .where(
+                        BOOK.AUTHOR_ID.equal(10L))
+                .and(
+                        BOOK.TITLE.startsWith("My")
+                                .or(BOOK.TITLE.startsWith("Your")))
+                .fetch();
+    }
+
+    /**
+     * insert some test records
+     * @param ctx the DSL Context to use
+     */
+    private void insertSomeValues(DSLContext ctx) {
+        ctx.insertInto(AUTHOR, AUTHOR.ID, AUTHOR.FIRSTNAME, AUTHOR.LASTNAME, AUTHOR.CATEGORY)
+                .values(10L, "Harry", "Hirsch", "Fiction").execute();
+
+        ctx.insertInto(ACCOUNT, ACCOUNT.ID, ACCOUNT.ACCOUNTNUMBER, ACCOUNT.ACCOUNTOWNER_ID, ACCOUNT.AMOUNT)
+                .values(11L, "SAVINGS001", 10L, 1500.00).execute();
+
+        ctx.insertInto(BOOK, BOOK.ID, BOOK.AUTHOR_ID, BOOK.TITLE, BOOK.PRICE, BOOK.CATEGORY)
+                .values(12L, 10L, "My little farm", 145.00, "Poetry").execute();
+
+        ctx.insertInto(BOOK, BOOK.ID, BOOK.AUTHOR_ID, BOOK.TITLE, BOOK.PRICE, BOOK.CATEGORY)
+                .values(13L, 10L, "Your little farm", 146.00, "Poetry").execute();
+    }
+
+    /**
+     * Simply execute a SQL DELETE
+     * @param ctx the DSL Context to use
+     */
+    private void deleteAll(DSLContext ctx) {
+        ctx.deleteFrom(BOOK).execute();
+        ctx.deleteFrom(ACCOUNT).execute();
+        ctx.deleteFrom(AUTHOR).execute();
     }
 }
